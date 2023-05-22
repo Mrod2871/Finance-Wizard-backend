59809e82b4c4149e8bf6b29a6717263973e25462
Mario <mario2603@Ubuntu-Please.myguest.virtualbox.org>
2023-05-22 18:42:21 -0400

    Changes boh schemas number to be called amount

--- models/expense.js
+++ models/expense.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const ExpenseSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    number: {type: Number, required: true},
+    amount: {type: Number, required: true},
 })
 
 const Expense = mongoose.model("Expense", ExpenseSchema)
--- models/income.js
+++ models/income.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const IncomeSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    number: {type: Number, required: true},
+    amount: {type: Number, required: true},
 })
 
 const Income = mongoose.model("Income", IncomeSchema)
